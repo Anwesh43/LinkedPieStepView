0f24446e6386fbc1887949517e29764a5669dc1d
Anwesh43 <anweshthecool0@gmail.com>
2018-09-17 02:14:56 +0530

    fullScreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedpiestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedpiestepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedpiestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.piestepview.PieStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         PieStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
