1380cd6b9974a56cf6ea2beea153fc2288d0b8e2
Anwesh43 <anweshthecool0@gmail.com>
2018-09-17 01:43:11 +0530

    adding libraries and defining nodes

--- piestepview/src/main/java/com/anwesh/uiprojects/piestepview/PieStepView.kt
+++ piestepview/src/main/java/com/anwesh/uiprojects/piestepview/PieStepView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.piestepview
 /**
  * Created by anweshmishra on 17/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
